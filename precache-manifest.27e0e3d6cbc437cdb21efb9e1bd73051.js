780ed305f7815a1b3aab314fa4d74c59557b041d
avneet kaur <avneetkaur3105@gmail.com>
2020-04-29 22:29:00 +0530

    Updates

---
+++ precache-manifest.27e0e3d6cbc437cdb21efb9e1bd73051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d16b3712b87b0d286052299740a115c",
+    "url": "/reactfoodapp/index.html"
+  },
+  {
+    "revision": "978797e91a95a6351528",
+    "url": "/reactfoodapp/static/css/main.50f2fa6e.chunk.css"
+  },
+  {
+    "revision": "7e93996d0d3e5a978794",
+    "url": "/reactfoodapp/static/js/2.b966dc89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactfoodapp/static/js/2.b966dc89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978797e91a95a6351528",
+    "url": "/reactfoodapp/static/js/main.332c1286.chunk.js"
+  },
+  {
+    "revision": "20bfc2403a4c70e0f5a1",
+    "url": "/reactfoodapp/static/js/runtime-main.139c3548.js"
+  }
+]);
